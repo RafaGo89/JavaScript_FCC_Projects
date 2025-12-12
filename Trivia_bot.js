d16fcad28a5dd8782030d6d7ce6a4a8661e24c6b
Rafael Rodríguez Gómez <135170128+RafaGo89@users.noreply.github.com>
2025-12-11 23:10:40 -0600

    Add files via upload

---
+++ Trivia_bot.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+// Variables
+let botName = "chabot";
+let botLocation = "Mexico";
+let favoriteLanguage = "Perl";
+let  codingFact = "\nA fun fact about Perl is that its unofficial symbol is a dromedary camel, which became popular after appearing on the cover of the authoritative book, Programming Perl. The book is widely known as the 'Camel Book'.";
+
+console.log("My name is " + botName + " and I live on "+ botLocation + ".");
+
+console.log("My favorite programming language is "+ favoriteLanguage + ".")
+
+console.log(codingFact);
+
+codingFact = '\nThe name "Perl" was originally "Pearl", inspired by the Biblical "Parable of the Pearl," but the "a" was dropped because a programming language with that name already existed.';
+
+console.log(codingFact);
+
+codingFact = '\n' + favoriteLanguage + ' is often called the "duct tape of the Internet" because it became a crucial "glue language" for connecting different systems and automating tasks in the early days of the web.';
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
---
+++ sentence_maker.js
@@ -0,0 +1,22 @@
+// Variables
+let adjective = "handsome";
+let noun = "duck";
+let verb = "sleeping";
+let place = "rock";
+let adjective2 = "ugly";
+let noun2 = "trees";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "fast";
+noun = "mountain";
+verb = "dancing";
+place = "basement";
+adjective2 = "sturdy";
+noun2 = "pineapples";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
